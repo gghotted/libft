0c785a3eb5e51c8755f5554968c767ce440f696a
Gyoungin Park <gypark@c2r1s3.42seoul.kr>
2020-10-04 06:09:21 +0900

    edit ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: gypark <gypark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 05:36:34 by gypark            #+#    #+#             */
-/*   Updated: 2020/10/04 05:45:37 by gypark           ###   ########.fr       */
+/*   Updated: 2020/10/04 06:08:13 by gypark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strdup(const char *s)
 	char	*dup_org;
 	char	*dup;
 
-	dup = malloc(ft_strlen(s));
+	dup = malloc(ft_strlen(s) + 1);
 	dup_org = dup;
 	while (*s)
 		*dup++ = *s++;
